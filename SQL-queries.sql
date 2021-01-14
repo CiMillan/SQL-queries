9d4c49972cdf9727724858f5cd02ab7a43fa29b4
CiMillan <hi@cintiamillan.com>
2021-01-14 16:27:11 +0000

    added entity relationship diagram

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -134,6 +134,70 @@ SELECT name
 FROM accounts
 WHERE name NOT LIKE '%s';
 
+/*Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.*/
+
+SELECT standard_qty, poster_qty, gloss_qty
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
+/*Using the accounts table, find all the companies whose names do not start with 'C' and end with 's'.*/
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name LIKE '%s';
+
+/*When you use the BETWEEN operator in SQL, do the results include the values of your endpoints, or not? */
+
+yes, the BETWEEN operator in SQL is inclusive
+
+/*writing a query that displays the order date and gloss_qty data for all orders where gloss_qty is between 24 and 29.*/
+
+SELECT occurred_at, gloss_qty
+FROM orders
+WHERE gloss_qty BETWEEN 24 AND 29;
+
+/*Use the web_events table to find all information regarding individuals who were contacted via the organic or adwords channels, and started their account at any point in 2016, sorted from newest to oldest.*/
+/*While BETWEEN is generally inclusive of endpoints, it assumes the time is at 00:00:00 (i.e. midnight) for dates. This is the reason why we set the right-side endpoint of the period at '2017-01-01'.*/
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+/*Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table.*/
+
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000
+
+/*Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.*/
+
+SELECT *
+FROM orders
+WHERE (gloss_qty > 1000 OR poster_qty > 1000)
+	AND standard_qty = 0;
+
+/*Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.*/
+
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%')
+	AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
+    AND primary_poc NOT LIKE '%eana%');
+
+/*pulling all the data from the accounts table, and all the data from the orders table.*/
+
+SELECT orders.*, accounts.*
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id;
+
+/*pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table.*/
+
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.website, accounts.primary_poc
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id
 
 
 
