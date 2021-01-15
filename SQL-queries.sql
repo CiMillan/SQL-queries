9050b33df780a897277e3d59735490e4b0ba839f
CiMillan <hi@cintiamillan.com>
2021-01-15 12:09:06 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -309,6 +309,51 @@ JOIN web_events w
 
 /*Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at, account name, order total, and order total_amt_usd.*/
 
+SELECT o.occurred_at date, a.name account, o.total total, o.total_amt_usd usd
+FROM orders o
+JOIN accounts a
+	ON a.id = o.account_id
+    WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
+ORDER BY o.occurred_at DESC;
+
+
+/********************SQL AGREGATIONS************************/
+
+
+/*Find the total amount of poster_qty paper ordered in the orders table.*/
+
+SELECT SUM(poster_qty) AS poster
+FROM orders;
+
+/*Find the total amount of standard_qty paper ordered in the orders table.*/
+
+SELECT SUM(standard_qty) AS standard
+FROM orders;
+
+/*Find the total dollar amount of sales using the total_amt_usd in the orders table.*/
+
+SELECT SUM(total_amt_usd) AS total
+FROM orders;
+
+/*Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table.*/
+
+SELECT (standard_amt_usd + gloss_amt_usd) AS total_standard_gloss
+FROM orders;
+
+/*Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both an aggregation and a mathematical operator.*/
+
+SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS unit_price_standard
+FROM orders;
+
+/*When was the earliest order ever placed? You only need to return the date.*/
+
+SELECT MIN(occurred_at)
+FROM orders;
+
+/*Try performing the same query as in question without using an aggregation function.*/
+
+
+
 
 
 
