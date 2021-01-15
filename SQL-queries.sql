3b84126209169673b089364a961b7992790218a7
CiMillan <hi@cintiamillan.com>
2021-01-15 14:36:39 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -352,6 +352,115 @@ FROM orders;
 
 /*Try performing the same query as in question without using an aggregation function.*/
 
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1;
+
+/*When did the most recent (latest) web_event occur?*/
+
+SELECT MIN(occurred_at)
+FROM web_events;
+
+/*Try to perform the result of the previous query without using an aggregation function.*/
+
+SELECT occurred_at
+FROM web_events
+ORDER BY occurred_at
+LIMIT 1;
+
+/*Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.*/
+
+SELECT 
+    AVG(standard_amt_usd) AS mean_usd_standard, 
+    AVG(gloss_amt_usd) AS mean_usd_gloss, 
+    AVG(poster_amt_usd) AS mean_usd_poster,
+    AVG(standard_qty) AS mean_qty_standard,
+    AVG(gloss_qty) AS mean_qty_gloss,
+    AVG(poster_qty) AS mean_qty_poster
+FROM orders;
+
+/*Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders? Note, this is more advanced than the topics we have covered thus far to build a general solution, but we can hard code a solution in the following way.*/
+/*ince there are 6912 orders - we want the average of the 3457 and 3456 order amounts when ordered. This is the average of 2483.16 and 2482.55. This gives the median of 2482.855. This obviously isn't an ideal way to compute. If we obtain new orders, we would have to change the limit. SQL didn't even calculate the median for us. The above used a SUBQUERY, but you could use any method to find the two necessary values, and then you just need the average of them.*/
+
+SELECT *
+FROM (SELECT total_amt_usd
+      FROM orders
+      ORDER BY total_amt_usd
+      LIMIT 3457) AS Table1
+ORDER BY total_amt_usd DESC
+LIMIT 2;
+
+/*Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.*/
+
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+	ON a.id = o.account_id
+ORDER BY occurred_at
+LIMIT 1;
+
+/*Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.*/
+
+SELECT a.name, SUM(total_amt_usd) total_orders_usd
+FROM accounts a
+JOIN orders o
+	ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY total_orders_usd DESC;
+
+/*Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.*/
+
+SELECT a.name, w.occurred_at, w.channel
+FROM accounts a
+JOIN web_events w
+	ON a.id = w.account_id
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+/*Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.*/
+
+SELECT w.channel, COUNT(*)
+FROM web_events w
+GROUP BY w.channel;
+
+/*Who was the primary contact associated with the earliest web_event?*/
+
+SELECT w.occurred_at, a.primary_poc
+FROM accounts a
+JOIN web_events w
+	ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+/*Who was the primary contact associated with the earliest web_event?*/
+
+SELECT w.occurred_at, a.primary_poc
+FROM accounts a
+JOIN web_events w
+	ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+/*What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.*/
+
+SELECT a.name, MIN(o.total_amt_usd) smallest_order
+FROM accounts a
+JOIN orders o
+	ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY smallest_order;
+
+/*Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.*/
+
+SELECT r.name, COUNT(s) reps
+FROM sales_reps s
+JOIN region r
+	ON r.id = s.region_id
+GROUP BY r.name
+ORDER BY reps;
+
+
 
 
 
