7c9046958f5573bc729b5f09f8152579215d4b28
CiMillan <hi@cintiamillan.com>
2021-01-18 16:16:02 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -949,10 +949,23 @@ ORDER BY 2 DESC;
 
 /*Use the accounts table and a CASE statement to create two groups: one group of company names that start with a number and a second group of those company names that start with a letter. What proportion of company names start with a letter?*/
 
+WITH t1 AS (
+	SELECT CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') THEN 0 ELSE 1 END AS num,
+    CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') THEN 1 ELSE 0 END AS letter
+  	FROM accounts) 
 
+SELECT SUM(num) AS total_num, SUM(letter) AS total_letter
+FROM t1;
 
 /*Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel, and what percent start with anything else?*/
 
+WITH t1 AS (
+	SELECT CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 0 ELSE 1 END AS other,
+    CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 1 ELSE 0 END AS vowel
+  	FROM accounts) 
+
+SELECT SUM(other) AS total_other, SUM(vowel) AS total_vowel
+FROM t1;
 
 
 
