414ccfd5ad4dba5818c18e80c9d7b4d82a895d00
CiMillan <hi@cintiamillan.com>
2021-01-19 10:43:33 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -967,6 +967,22 @@ WITH t1 AS (
 SELECT SUM(other) AS total_other, SUM(vowel) AS total_vowel
 FROM t1;
 
+/*Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc.*/
+
+SELECT primary_poc, 
+	POSITION(' ' IN primary_poc) AS space_position,
+    LEFT(primary_poc, POSITION(' ' IN primary_poc) - 1) AS first_name,
+    RIGHT(primary_poc, LENGTH(primary_poc) - POSITION(' ' IN primary_poc)) AS last_name
+FROM accounts;
+
+/*Now see if you can do the same thing for every rep name in the sales_reps table. Again provide first and last name columns.*/
+
+SELECT name,
+	POSITION(' ' IN name) AS space_position,
+    LEFT(name, POSITION(' ' IN name) - 1) AS first_name,
+    RIGHT(name, LENGTH(name) - POSITION(' ' IN name)) AS last_name
+FROM sales_reps;
+
 
 
 
