12094970a85bbc1611f85ff3a15179f6f931d04d
CiMillan <hi@cintiamillan.com>
2021-01-19 12:39:27 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -1082,12 +1082,22 @@ SELECT standard_amt_usd,
 SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
 FROM orders;
 
-/*with date truncation by month*/
+/*Now, modify your query from the previous quiz to include partitions. Still create a running total of standard_amt_usd (in the orders table) over order time, but this time, date truncate occurred_at by year and partition by that same year-truncated occurred_at variable. Your final table should have three columns: One with the amount being added for each row, one for the truncated date, and a final column with the running total within each year.*/
 
 SELECT standard_amt_usd,
-DATE_TRUNC('month', occurred_at) AS month,
-SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('month', occurred_at) ORDER BY occurred_at) AS running_total
+DATE_TRUNC('year', occurred_at) AS year,
+SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
 FROM orders;
+
+/*Select the id, account_id, and total variable from the orders table, then create a column called total_rank that ranks this total amount of paper ordered (from highest to lowest) for each account using a partition. Your final table should have these four columns.*/
+
+SELECT id,
+	account_id,
+    total,
+    RANK() OVER (PARTITION BY account_id ORDER BY total DESC) AS total_rank
+FROM orders;
+
+
 
 
 
