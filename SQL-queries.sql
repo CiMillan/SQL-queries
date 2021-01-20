e8ddc471920367a3b08f9a27bd1c838d3b40564d
CiMillan <hi@cintiamillan.com>
2021-01-20 11:56:38 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -1213,7 +1213,57 @@ SELECT *
  FULL JOIN sales_reps ON accounts.sales_rep_id = sales_reps.id
  WHERE accounts.sales_rep_id IS NULL OR sales_reps.id IS NULL;
 
- 
+ /*write a query that left joins the accounts table and the sales_reps tables on each sale rep's ID number and joins it using the < comparison operator on accounts.primary_poc and sales_reps.name*/
+
+ SELECT a.name account, a.primary_poc poc, s.name rep
+FROM accounts a
+LEFT JOIN sales_reps s
+	ON a.sales_rep_id = s.id
+    AND a.primary_poc < s.name;
+
+/*Modify the query from the previous video, which is pre-populated in the SQL Explorer below, to perform the same interval analysis except for the web_events table. Also:
+
+change the interval to 1 day to find those web events that occurred after, but not more than 1 day after, another web event
+add a column for the channel variable in both instances of the table in your query*/
+
+SELECT we1.id AS we_id,
+       we1.account_id AS we1_account_id,
+       we1.occurred_at AS we1_occurred_at,
+       we1.channel AS we1_channel,
+       we2.id AS we2_id,
+       we2.account_id AS we2_account_id,
+       we2.occurred_at AS we2_occurred_at,
+       we2.channel AS we2_channel
+  FROM web_events we1 
+ LEFT JOIN web_events we2
+   ON we1.account_id = we2.account_id
+  AND we1.occurred_at > we2.occurred_at
+  AND we1.occurred_at <= we2.occurred_at + INTERVAL '1 day'
+ORDER BY we1.account_id, we2.occurred_at;
+
+/*Write a query that uses UNION ALL on two instances (and selecting all columns) of the accounts table. Then inspect the results and answer the subsequent quiz.*/
+
+SELECT *
+    FROM accounts
+
+UNION ALL
+
+SELECT *
+  FROM accounts
+
+/*Add a WHERE clause to each of the tables that you unioned in the query above, filtering the first table where name equals Walmart and filtering the second table where name equals Disney. Inspect the results then answer the subsequent quiz.*/
+
+SELECT *
+    FROM accounts a1
+    WHERE a1.name = 'Walmart'
+
+UNION ALL
+
+SELECT *
+  FROM accounts a2
+  WHERE a2.name = 'Disney'
+
+  
 
 
 
