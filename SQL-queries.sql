41bb8598ca73b111ff3a572d2447b31ec4fb5597
CiMillan <hi@cintiamillan.com>
2021-01-22 11:16:49 +0000

    updated queries

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -1263,9 +1263,18 @@ SELECT *
   FROM accounts a2
   WHERE a2.name = 'Disney'
 
-  
+/*Perform the union in your first query (under the Appending Data via UNION header) in a common table expression and name it double_accounts. Then do a COUNT the number of times a name appears in the double_accounts table. If you do this correctly, your query results should have a count of 2 for each name.*/
 
+WITH double_accounts AS (
+  SELECT *
+    FROM accounts
+UNION ALL 
+SELECT *
+  FROM accounts)
 
+SELECT name, COUNT(*) AS name_count
+FROM double_accounts
+GROUP BY 1;
 
 
 
